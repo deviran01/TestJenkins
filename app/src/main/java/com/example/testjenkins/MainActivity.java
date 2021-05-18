1823f837000433fe2c071cc8bf0955b3c2984507
Ali <ali.sarkhosh@cafebazaar.ir>
2021-05-18 14:24:49 +0430

    init commit

---
+++ app/src/main/java/com/example/testjenkins/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testjenkins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Clicked");
+            }
+        });
+
+    }
+}
